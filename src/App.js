761c3cd69b013f2df27a70d90d13a2694261790c
Marian <hellomariandev@gmail.com>
2022-08-17 22:17:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header } from "components/Header";
+
+import { CssBaseline, Box, Container } from "@mui/material";
+
+import OptionsTab from "components/OptionsTab";
+import LocationCards from "components/LocationCards";
+import Footer from "components/Footer";
+import "./App.css";
+import FooterMenu from "components/FooterMenu";
+import { displayOnDesktop } from "theme/commonStyles";
+import MobileFooter from "components/MobileFooter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <CssBaseline />
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        <Box>
+          <Header />
+          <OptionsTab />
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            height: "100%",
+            overflowY: "scroll",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Container maxWidth="xl" sx={{ md: 3 }}>
+            <LocationCards />
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <MobileFooter />
+            </Box>
+          </Container>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <FooterMenu />
+        </Box>
+        <Box sx={displayOnDesktop}>
+          <Footer />
+        </Box>
+      </Box>
+    </React.Fragment>
   );
 }
 
