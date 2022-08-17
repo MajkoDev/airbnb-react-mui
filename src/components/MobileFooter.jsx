0aa4c1db9250f4f5be544edbba9d24f1d255d400
Marian <hellomariandev@gmail.com>
2022-08-17 22:17:36 +0200

    footer

---
+++ src/components/MobileFooter.jsx
@@ -0,0 +1,38 @@
+import { Box, Paper, Button, Stack, Link } from "@mui/material";
+import { justifyCenter } from "theme/commonStyles";
+import { BsGlobe } from "react-icons/bs";
+
+const footerLinks = [
+    { id: 1, text: "Privacy", url: "#" },
+    { id: 2, text: "Terms", url: "#" },
+    { id: 3, text: "Sitemap", url: "#" }  ];
+
+const MobileFooter = () => {
+  return (
+    <Box sx={{ borderTop: "1px solid #ccc", mt: 3, pt: 2, width: "100%" }}>
+      <Paper>
+        <Button>
+          <Box sx={{ ...justifyCenter, mr: 1 }}>
+            <BsGlobe size={24} />
+          </Box>
+          English ( CA )
+        </Button>
+        <Button>$ CAD</Button>
+      </Paper>
+      <Stack sx={{ mt: 2 }}>
+        <Paper>
+          <Link href="#">2022 Airbnb Copyright</Link>
+        </Paper>
+      </Stack>
+      <Stack sx={{ mt: 2 }}>
+        {footerLinks.map((link) => {
+          <Paper key={link.id}>
+            <Link href={link.url}>{link.text}</Link>
+          </Paper>;
+        })}
+      </Stack>
+    </Box>
+  );
+};
+
+export default MobileFooter;
